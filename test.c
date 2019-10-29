13d0af68fc5c67269d30ef2236f61848bebaaf0f
hyw1573753761 <1573753761@qq.com>
2019-10-29 09:20:51 +0800

    1.输出100到200之间的素数 2.输出1000到2000之间的闰年

---
+++ test.c
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main() {
+	int num, i;
+	for (num = 100; num <= 200; ++num) {
+		for (i = 2; i <= num; ++i) {
+			if (num%i == 0) {
+				break;
+			}
+		}
+		if (num == i){
+			printf("%d\n", num); 
+		}
+	}
+	system("pause");
+	return 0;
+}
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int leap(int year)
+{
+	int ret  = 0;;
+	if (((year % 4 == 0)
+		&& (year % 100 != 0))
+		|| (year % 400 == 0))
+	{
+		return 1;
+	}
+	else
+	{
+	return - 1;
+	}
+}
+
+int main()
+{
+	int Year = 0;
+	for (Year = 1000; Year < 2001; ++Year) {
+		int ret = leap(Year);/*将调用得到的函数值返回到ret中*/
+		if (ret == 1)
+		{
+			printf("%d\t", Year);
+		}
+	}
+	system("pause");
+	return 0;
+}
